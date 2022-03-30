ef36069be6b406f00f7b33954a644a9cca85f8bc
Daniel Calvin <arockne2003@yahoo.com>
2022-03-29 19:18:13 -0700

    commit tree 2f796fb7bc272ad993156f979da76596f716091f

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,15 @@
 // write your CatList component here
+
+function CatList({ catPics }) {
+  return (
+    <div>
+      {
+        catPics.map((pic) => {
+          return <img key={pic.id} src={pic.url} alt="cat" width="300"/>
+        })
+      }
+    </div>
+  )
+}
+
+export default CatList
